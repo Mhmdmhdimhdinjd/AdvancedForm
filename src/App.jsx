07f6d8fc8a61227fd574add67a2d9304b6ff3eb1
mhmd mhdi <mhdimhdin5@gmail.com>
2025-03-17 17:47:49 +0330

    chnge navbar component address

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Controller, useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import Form from "./components/form";
-import Navbar__comp from "./components/Navbar";
+import Navbar__comp from "./components/navbar";
 import Asidebar from "./components/aside";
 import TableComp from "./components/table/container.jsx";
 import { useDispatch } from "react-redux";
